d4cb9795876b6af316bc9f5bad84485f85402a55
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2023-06-13 16:46:35 +0200

    login e register layout

---
+++ app/src/main/java/com/zimp/zimpcarsharing/DBManager.kt
@@ -0,0 +1,8 @@
+package com.zimp.zimpcarsharing
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+
+class DBManager(val context: Context){
+
+}
--- app/src/main/java/com/zimp/zimpcarsharing/MainActivity.kt
+++ app/src/main/java/com/zimp/zimpcarsharing/MainActivity.kt
@@ -2,10 +2,13 @@ package com.zimp.zimpcarsharing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zimp.zimpcarsharing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
