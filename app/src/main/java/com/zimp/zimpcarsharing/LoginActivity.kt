dd9bf5f4813f9b4ea6126af0c4d4fa97c0117dcb
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2023-06-14 13:53:40 +0200

    inizio sclero retrofit 2

--- app/src/main/java/com/zimp/zimpcarsharing/LoginActivity.kt
+++ app/src/main/java/com/zimp/zimpcarsharing/LoginActivity.kt
@@ -1,13 +1,15 @@
 package com.zimp.zimpcarsharing
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.zimp.zimpcarsharing.databinding.LoginBinding
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
-import javax.security.auth.callback.Callback
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: LoginBinding
@@ -24,20 +26,33 @@ class LoginActivity : AppCompatActivity() {
 
     private fun loginUtente(){
         val query = "Select * from persona"
-        /**
-        ClientNetwork.retrofit.login(query).enqueue(
 
-            object : Callback <JsonObject>{
-                fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>){
-                    
+        ClientNetwork.retrofit.login(query).enqueue(
+            object : Callback<JsonObject> {
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    if (response.isSuccessful) {
+                        if ((response.body()?.get("queryset") as JsonArray).size() == 1) {
+                            //getImageProfilo((response.body()?.get("queryset") as JsonArray).get(0) as JsonObject)
+                        } else {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "credenziali errate",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            //binding.progressBar.visibility = View.GONE
+                        }
+                    }
                 }
-                fun onFailure(call: Call<JsonObject>, t : Throwable){
+
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    /*
+                     * gestisci qui il fallimento della richiesta
+                     */
 
                 }
             }
-
         )
-        */
+
     }
 
 }
